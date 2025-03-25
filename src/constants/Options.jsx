f787ccc48a3b700436813c0aa6423579841ec22c
Piyush Garg <piyushgarg809@gmail.com>
2025-03-25 23:11:13 +0530

    work on progress

---
+++ src/constants/Options.jsx
@@ -0,0 +1,52 @@
+export const SelectTravelesList =[
+        {
+          id: 1,
+          title: "Just Me",
+          desc: "A solo travel in exploration",
+          icons: "✈️",
+          people: "1"
+        },
+        {
+          id: 2,
+          title: "Couple Getaway",
+          desc: "A romantic escape for two",
+          icons: "❤️",
+          people: "2 people"
+        },
+        {
+          id: 3,
+          title: "Family Vacation",
+          desc: "A fun-filled adventure with family",
+          icons: "👨‍👩‍👧‍👦",
+          people: "3 to 5 people"
+        },
+        {
+          id: 4,
+          title: "Friends Trip",
+          desc: "An exciting journey with friends",
+          icons: "🎉",
+          people: "5 to 8 people"
+        }
+]
+
+export const SelectBudgetOptions =[
+    {
+      id: 1,
+      title: "Cheap",
+      desc: "Stay conscious of cost",
+      icons: "💸"
+    },
+    {
+      id: 2,
+      title: "Moderate",
+      desc: "Keep cost on the average side",
+      icons: "💰"
+    },
+    {
+      id: 3,
+      title: "Luxury",
+      desc: "Dont worry about cost",
+      icons: "👑"
+    }
+  ]
+  
--- src/create-trip/Index.jsx
+++ src/create-trip/Index.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input } from "@/components/ui/input"
+
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { SelectBudgetOptions } from "@/constants/Options";
 
 const CreateTrip = () => {
+  const [place, setPlace] = useState();
   return (
     <div className="sm:px-10 md:px-32 lg:56 xl:px-72 px-5 mt-10 ">
       <h2 className="font-bold text-3xl">
@@ -12,13 +16,39 @@ const CreateTrip = () => {
         a <br /> customized itinerary based on your preferences.
       </p>
 
-      <div className="mt-20">
+      <div className="mt-20 flex flex-col gap-10 text-left max-w-2/3">
         <div>
           <h2 className="text-xl my-3 font-medium">What is destination of choice ?</h2>
 
           <GooglePlacesAutocomplete 
            apiKey={import.meta.env.VITE_GEOAPIFY_API_KEY}
+
+           selectProps={{
+            place ,
+            onChange:(v) => {setPlace(v) ; console.log(v);
+            }
+           }}
           />
+        </div>
+
+        <div>
+        <h2 className="text-xl my-3 font-medium">How many days are you planning your trip ?</h2>
+
+         <Input placeholder="Ex -3" type="number" />
+
+        </div>
+
+        <div>
+        <h2 className="text-xl my-3 font-medium">What is Your Budget ?</h2>
+         <div className='grid grid-cols-3 gap-5 mt-5 '>
+          {SelectBudgetOptions.map((item , index) =>(
+            <div key={index} className="p-4 border rounded-lg hover:shadow-lg">
+                  <h2>{item.icons}</h2>
+                  <h2>{item.title}</h2>
+                  <h2>{item.desc}</h2>
+            </div>
+          ))}
+         </div>
         </div>
       </div>
     </div>
