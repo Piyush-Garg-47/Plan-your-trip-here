7fb9cf3bbb029a9b128af9bd3f7136067f48c5f2
Piyush Garg <piyushgarg809@gmail.com>
2025-03-25 23:31:02 +0530

    25 -03-2025

--- src/create-trip/Index.jsx
+++ src/create-trip/Index.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Input } from "@/components/ui/input"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
-import { SelectBudgetOptions } from "@/constants/Options";
+import { SelectBudgetOptions  , SelectTravelesList} from "@/constants/Options";
 
 const CreateTrip = () => {
   const [place, setPlace] = useState();
@@ -18,38 +19,65 @@ const CreateTrip = () => {
 
       <div className="mt-20 flex flex-col gap-10 text-left max-w-2/3">
         <div>
-          <h2 className="text-xl my-3 font-medium">What is destination of choice ?</h2>
+          <h2 className="text-xl my-3 font-medium">
+            What is destination of choice ?
+          </h2>
 
-          <GooglePlacesAutocomplete 
-           apiKey={import.meta.env.VITE_GEOAPIFY_API_KEY}
-
-           selectProps={{
-            place ,
-            onChange:(v) => {setPlace(v) ; console.log(v);
-            }
-           }}
+          <GooglePlacesAutocomplete
+            apiKey={import.meta.env.VITE_GEOAPIFY_API_KEY}
+            selectProps={{
+              place,
+              onChange: (v) => {
+                setPlace(v);
+                console.log(v);
+              },
+            }}
           />
         </div>
 
         <div>
-        <h2 className="text-xl my-3 font-medium">How many days are you planning your trip ?</h2>
+          <h2 className="text-xl my-3 font-medium">
+            How many days are you planning your trip ?
+          </h2>
 
-         <Input placeholder="Ex -3" type="number" />
-
+          <Input placeholder="Ex -3" type="number" />
         </div>
 
         <div>
-        <h2 className="text-xl my-3 font-medium">What is Your Budget ?</h2>
-         <div className='grid grid-cols-3 gap-5 mt-5 '>
-          {SelectBudgetOptions.map((item , index) =>(
-            <div key={index} className="p-4 border rounded-lg hover:shadow-lg">
-                  <h2>{item.icons}</h2>
-                  <h2>{item.title}</h2>
-                  <h2>{item.desc}</h2>
-            </div>
-          ))}
-         </div>
+          <h2 className="text-xl my-3 font-medium">What is Your Budget ?</h2>
+          <div className="grid grid-cols-3 gap-5 mt-5 ">
+            {SelectBudgetOptions.map((item, index) => (
+              <div
+                key={index}
+                className="p-4 border rounded-lg  hover:shadow-lg cursor-pointer"
+              >
+                <h2 className="text-4xl">{item.icons}</h2>
+                <h2 className="font-bold text-lg">{item.title}</h2>
+                <h2 className="text-sm text-gray-500">{item.desc}</h2>
+              </div>
+            ))}
+          </div>
         </div>
+
+        <div>
+          <h2 className="text-xl my-3 font-medium">Who do you plan on traveling with on your next adventure ?</h2>
+          <div className="grid grid-cols-3 gap-5 mt-5 ">
+            {SelectTravelesList.map((item, index) => (
+              <div
+                key={item.id}
+                className="p-4 border rounded-lg  hover:shadow-lg cursor-pointer"
+              >
+                <h2 className="text-4xl">{item.icons}</h2>
+                <h2 className="font-bold text-lg">{item.title}</h2>
+                <h2 className="text-sm text-gray-500">{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+
+         <div  className="my-10 justify-end flex">
+          <Button>Generate Trip</Button>
+         </div>
       </div>
     </div>
   );
